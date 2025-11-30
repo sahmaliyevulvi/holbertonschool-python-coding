d21ff1ae235a7cfa56399002c826269420517e07
root <root@ip-10-42-153-197.ec2.internal>
2025-11-30 10:26:25 +0000

    message

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square shape."""
+    def __init__(self, size):
+        """returns the size of square."""
+        self.__size = size
