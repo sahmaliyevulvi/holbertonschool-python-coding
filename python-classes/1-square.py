de7e0652027548581a2ab59faf7fd86d57613e11
root <root@ip-10-42-153-197.ec2.internal>
2025-11-30 10:29:53 +0000

    message

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a square shape."""
+    def __init__(self, size=0):
+        """returns the size of square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
